e56055d287e97ff30dd17933f72a8c22822105ed
Jonathan Singer <jsinger@stuy.edu>
2018-01-13 22:23:14 -0500

    updated src.js

--- snow/src.js
+++ snow/src.js
@@ -7,11 +7,18 @@ class Flake{
         this.position = createVector(x,y);
         this.velocity = createVector();
         this.acceleration = createVector();
-        this.r = constrain((pow(random(),10)*16),2,16);
+        this.angle = random(TWO_PI);
+        this.dir = random()<.5?1:-1;
+        this.xOffset = 0;
+        this.r = constrain((pow(random(),3)*16),2,16);
     }
     render(){
+        push();
+        translate(this.position.x+this.xOffset,this.position.y);
+        rotate(this.angle);
         imageMode(CENTER);
-        image(this.img,this.position.x,this.position.y,this.r,this.r);
+        image(this.img,0,0,this.r,this.r);
+        pop();
     }
     applyForce(force){
         let f = force.copy();
@@ -19,6 +26,7 @@ class Flake{
         this.acceleration.add(force);
     }
     update(){
+        this.xOffset = sin(this.angle)*this.r*1.5;
         this.velocity.add(this.acceleration);
         this.velocity.limit(this.r*.2);
         if(this.velocity.mag() <1){
@@ -29,6 +37,12 @@ class Flake{
         if(this.position.y>height + this.r){
             this.randomize();
         }
+        if(this.position.x<-this.r)
+            this.position.x = width + this.r;
+        if(this.position.x>width+this.r)
+            this.position.x = this.width - this.r;
+        this.angle += this.velocity.mag() / 150 * this.dir;
+        
     }
     offScreen(){
         return (this.position.y > height + this.r);
@@ -39,16 +53,17 @@ class Flake{
         this.position = createVector(x,y);
         this.velocity = createVector();
         this.acceleration = createVector();
-        this.r = constrain((pow(random(),10)*32),2,32);
+        this.img = random(textures);
+        this.r = constrain((pow(random(),4)*32),2,32);
     }
 }
-let snow=[],gravity;
+let snow=[],gravity,zOff=0;
 let spritesheet,textures = [];
 function preload(){
     spritesheet = loadImage('snowflakes.png');
 }
 function setup(){
-    gravity = createVector(0,0.03);
+    gravity = createVector(0,0.05);
     createCanvas(windowWidth,windowHeight);
     for(let x = 0;x<spritesheet.width;x+=32){
         for(let y = 0;y<spritesheet.height;y+=y+32){
@@ -56,7 +71,7 @@ function setup(){
             textures.push(img);
         }
     }
-    for(let i = 0;i<300;i++){
+    for(let i = 0;i<600;i++){
         let x=random(width);
         let y=random(height);
         let design = random(textures);
@@ -65,9 +80,16 @@ function setup(){
 }
 function draw(){
     background(0);
-    // snow.push(new Flake());
+    
+    zOff += .1;
     for(fl of snow){
+        let xOff = fl.position.x/width;
+        let yOff = fl.position.y/height;
+        let wAngle = noise(xOff,yOff,zOff) * TWO_PI;
+        let wind = createVector(cos(wAngle),sin(wAngle));
+        wind.mult(0.01);
         fl.applyForce(gravity);
+        fl.applyForce(wind);
         fl.update();
         fl.render();
     }
